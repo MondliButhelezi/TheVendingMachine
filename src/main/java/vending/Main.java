00b5a6c54cb32e076cbd11fcf8620a060776f53b
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-07-24 22:29:19 +0200

    added Main class

---
+++ src/main/java/vending/Main.java
@@ -0,0 +1,28 @@
+package vending;
+
+import vending.product.Chocolates;
+import vending.product.Product;
+import vending.product.SaltySnacks;
+import vending.product.SoftDrinks;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OverloadedVendingMachine overloadedVendingMachine = new OverloadedVendingMachine(5,6,7);
+        Product product = new Product();
+        SoftDrinks softDrinks = new SoftDrinks();
+        SaltySnacks saltySnacks = new SaltySnacks();
+        Chocolates chocolates = new Chocolates();
+
+        System.out.println("---------  You have Entered Vuyo's Vending Machine ---------- \n" +
+                "---------- This is what is available in stock-------\n" +
+                "1. Soft Drink - R10.00\n" +
+                "2. Salty Snacks - R12.00\n" +
+                "3. Chocolate - R15.00");
+
+        System.out.println(overloadedVendingMachine.chocolatesQty);
+        System.out.println(overloadedVendingMachine.saltySnacksQty);
+        System.out.println(overloadedVendingMachine.softDrinkQty);
+    }
+
+}
