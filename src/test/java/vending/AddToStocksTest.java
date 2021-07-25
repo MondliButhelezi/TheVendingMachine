2c5abd56389f343ab552140d23fd2a3300c110b4
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-07-26 01:08:46 +0200

    fixed addstock test that were failing

--- src/test/java/vending/AddToStocksTest.java
+++ src/test/java/vending/AddToStocksTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
             OverloadedVendingMachine overloadedVendingMachine = new OverloadedVendingMachine(3,6,9);
             SaltySnacks saltySnack = new SaltySnacks();
             overloadedVendingMachine.addStock(saltySnack);
-            assertEquals(6,overloadedVendingMachine.getStockLevelForSaltySnacks(),"Should return number of items in stock for Salty snacks");
+            assertEquals(7,overloadedVendingMachine.getStockLevelForSaltySnacks(),"Should return number of items in stock for Salty snacks");
         }
 
         @Test
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
             OverloadedVendingMachine overloadedVendingMachine = new OverloadedVendingMachine(3,6,9);
             SoftDrinks softDrinks = new SoftDrinks();
             overloadedVendingMachine.addStock(softDrinks);
-            assertEquals(21,overloadedVendingMachine.getStockLevelForSoftDrinks(),"Should return the number of items in stock for Salty snacks");
+            assertEquals(4,overloadedVendingMachine.getStockLevelForSoftDrinks(),"Should return the number of items in stock for Soft drinks");
         }
 
         @Test
@@ -30,7 +30,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
             OverloadedVendingMachine overloadedVendingMachine = new OverloadedVendingMachine(3,6,9);
             Chocolates chocolates = new Chocolates();
             overloadedVendingMachine.addStock(chocolates);
-            assertEquals(11,overloadedVendingMachine.getStockLevelForChocolate(),"Should be able to return the number of items in stock for Salty snacks");
+            assertEquals(10,overloadedVendingMachine.getStockLevelForChocolate(),"Should return the number of items in stock for chocolates");
         }
 
         @Test
@@ -38,13 +38,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
             OverloadedVendingMachine overloadedVendingMachine = new OverloadedVendingMachine(3,6,9);
             Product product = new Product();
             overloadedVendingMachine.addStock(product);
-            assertEquals(38,overloadedVendingMachine.getStock(),"Should be able to return the number of items in stock for Salty snacks");
+            assertEquals(21,overloadedVendingMachine.getStock(),"Should return the number of items in stock for everything");
         }
 
         @Test
         public void shouldAddToStockAmountEntered() {
             OverloadedVendingMachine overloadedVendingMachine = new OverloadedVendingMachine(3,6,9);
             overloadedVendingMachine.addStock(5,5,5);
-            assertEquals(50,overloadedVendingMachine.getStock(),"Should be able to return the number of items in stock for Salty snacks");
+            assertEquals(33,overloadedVendingMachine.getStock(),"Should return the number of items entered");
         }
 }
