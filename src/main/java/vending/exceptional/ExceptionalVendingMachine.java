ab1c5d11eb1c983e07e3386b2dcd4a67af6b6258
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-07-29 23:16:56 +0200

    1st attempt of exceptional vending machine

---
+++ src/main/java/vending/exceptional/ExceptionalVendingMachine.java
@@ -0,0 +1,14 @@
+package vending.exceptional;
+
+public class ExceptionalVendingMachine {
+
+    private int softDrinksLevel = 0;
+    private int chocolatesLevel = 0;
+    private int saltySnacksLevel = 0;
+
+    public ExceptionalVendingMachine(int softDrinksLevel, int chocolatesLevel, int saltySnacksLevel) {
+        this.softDrinksLevel = softDrinksLevel;
+        this.chocolatesLevel = chocolatesLevel;
+        this.saltySnacksLevel = saltySnacksLevel;
+    }
+}
