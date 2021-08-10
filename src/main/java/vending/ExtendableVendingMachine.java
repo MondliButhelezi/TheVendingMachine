a7ddf6e177fcf0326b13452496b4a6303377a3b2
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-08-10 09:36:26 +0200

    instance variables case fix

--- src/main/java/vending/ExtendableVendingMachine.java
+++ src/main/java/vending/ExtendableVendingMachine.java
@@ -6,63 +6,63 @@ import vending.product.SaltySnacks;
 import vending.product.SoftDrinks;
 
 public class ExtendableVendingMachine {
-    private int SoftDrinks, SaltySnacks,Chocolates, stockType = 0;
+    private int softDrinks, saltySnacks, chocolates, stockType = 0;
 
-    public ExtendableVendingMachine(int SoftDrinks, int SaltySnacks, int Chocolates) {
-        this.SoftDrinks = SoftDrinks;
-        this.SaltySnacks = SaltySnacks;
-        this.Chocolates = Chocolates;
+    public ExtendableVendingMachine(int softDrinks, int saltySnacks, int chocolates) {
+        this.softDrinks = softDrinks;
+        this.saltySnacks = saltySnacks;
+        this.chocolates = chocolates;
     }
     public void buy(Product product) {
         if (product instanceof vending.product.SoftDrinks) {
-            SoftDrinks = SoftDrinks-1;
+            softDrinks = softDrinks -1;
         }
         if (product instanceof vending.product.SaltySnacks) {
-            SaltySnacks = SaltySnacks-1;
+            saltySnacks = saltySnacks -1;
         }
         if (product instanceof vending.product.Chocolates) {
-            Chocolates = Chocolates-1;
+            chocolates = chocolates -1;
         }
     }
 
     public void addStock(Product product) {
         if (product instanceof SoftDrinks) {
-            SoftDrinks = SoftDrinks+3;
+            softDrinks = softDrinks +3;
         }
         if (product instanceof SaltySnacks) {
-            SaltySnacks = SaltySnacks+3;
+            saltySnacks = saltySnacks +3;
         }
         if (product instanceof Chocolates) {
-            Chocolates = Chocolates+3;
+            chocolates = chocolates +3;
         }
     }
 
     public int getSoftDrinks() {
-        System.out.println("Stock level for SoftDrinks: [ " + SoftDrinks + " ]");
-        return SoftDrinks;
+        System.out.println("Stock level for SoftDrinks: [ " + softDrinks + " ]");
+        return softDrinks;
     }
 
     public int getChocolates() {
-        System.out.println("Stock level for Chocolates: [ " + Chocolates + " ]");
-        return Chocolates;
+        System.out.println("Stock level for Chocolates: [ " + chocolates + " ]");
+        return chocolates;
     }
 
     public int getSaltySnacks() {
-        System.out.println("stock level for SaltySnacks: [ " + SaltySnacks + " ]");
-        return SaltySnacks;
+        System.out.println("stock level for SaltySnacks: [ " + saltySnacks + " ]");
+        return saltySnacks;
     }
 
     public int getStock(Product product) {
         if (product instanceof SoftDrinks){
-            stockType = stockType + SoftDrinks;
+            stockType = stockType + softDrinks;
             System.out.println("SoftDrinks stock left is: [ " + stockType + " ]");
         }
         if (product instanceof SaltySnacks) {
-            stockType = stockType + SaltySnacks;
+            stockType = stockType + saltySnacks;
             System.out.println("Salty Snacks stock left is: [ " + stockType + " ]");
         }
         if (product instanceof Chocolates) {
-            stockType = stockType + Chocolates;
+            stockType = stockType + chocolates;
             System.out.println("Chocolates stock left is: [ " + stockType + " ]");
         }
 
@@ -71,25 +71,25 @@ public class ExtendableVendingMachine {
 
     public void buy(Product product, int qty) {
         if (product instanceof Chocolates) {
-            Chocolates = Chocolates - qty;
+            chocolates = chocolates - qty;
         }
         if (product instanceof SaltySnacks) {
-            SaltySnacks = SaltySnacks - qty;
+            saltySnacks = saltySnacks - qty;
         }
         if (product instanceof SoftDrinks) {
-            SoftDrinks = SoftDrinks - qty;
+            softDrinks = softDrinks - qty;
         }
     }
 
     public void addStock(Product product, int qty) {
         if (product instanceof Chocolates) {
-            Chocolates = Chocolates + qty;
+            chocolates = chocolates + qty;
         }
         if (product instanceof SaltySnacks) {
-            SaltySnacks = SaltySnacks + qty;
+            saltySnacks = saltySnacks + qty;
         }
         if (product instanceof SoftDrinks) {
-            SoftDrinks = SoftDrinks + qty;
+            softDrinks = softDrinks + qty;
         }
     }
 }
